b88656119cd71f7a77e11da622d96d9d9ac26603
Sumalya Chatterjee <105884682+R3DHULK@users.noreply.github.com>
2023-09-20 22:28:17 +0530

    Facebook Login Page Clone

---
+++ Facebook-Clone/script.js
@@ -0,0 +1,3 @@
+// popper in icon
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
